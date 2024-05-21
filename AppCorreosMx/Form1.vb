6d621c5b2ee09e6ff53422bfd926abbfaa269f3c
lesly <lesly@LESLY>
2024-05-20 18:03:15 -0600

    Agregar archivos de proyecto.

---
+++ AppCorreosMx/Form1.vb
@@ -0,0 +1,42 @@
+﻿Imports System.Data.SqlClient
+
+
+Public Class Form1
+    Private Sub btnConectar_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ' Cadena de conexión a la base de datos
+        Dim connectionString As String = "Data Source=LESLY;Initial Catalog=CorreosMx;Integrated Security=True"
+
+        ' Crear una nueva conexión
+        Dim connection As New SqlConnection(connectionString)
+
+        Try
+            ' Abrir la conexión
+            connection.Open()
+            MessageBox.Show("Conexión exitosa...")
+
+            ' Consulta SQL para obtener datos
+            Dim query As String = "SELECT * FROM cpdescarga"
+
+            ' Crear un adaptador de datos
+            Dim adapter As New SqlDataAdapter(query, connection)
+
+            ' Crear un DataSet para contener los datos
+            Dim dataSet As New DataSet()
+
+            ' Llenar el DataSet con los datos del adaptador
+            adapter.Fill(dataSet, "TablaDatos")
+
+            ' Mostrar los datos en el DataGridView
+            DataGridView1.DataSource = dataSet.Tables("TablaDatos")
+
+        Catch ex As Exception
+            MessageBox.Show("Error al conectar a la base de datos: " & ex.Message)
+        Finally
+            ' Cerrar la conexión cuando hayas terminado
+            If connection.State = ConnectionState.Open Then
+                connection.Close()
+            End If
+        End Try
+    End Sub
+End Class
+
---
+++ AppCorreosMx/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' La información general de un ensamblado se controla mediante el siguiente 
+' conjunto de atributos. Cambie estos valores de atributo para modificar la información
+' asociada con un ensamblado.
+
+' Revisar los valores de los atributos del ensamblado
+
+<Assembly: AssemblyTitle("AppCorreosMx")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("HP")>
+<Assembly: AssemblyProduct("AppCorreosMx")>
+<Assembly: AssemblyCopyright("Copyright © HP 2024")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+<Assembly: Guid("f7280ac6-7ae9-4b75-85ea-52828c46a8ae")>
+
+' La información de versión de un ensamblado consta de los cuatro valores siguientes:
+'
+'      Versión principal
+'      Versión secundaria
+'      Número de compilación
+'      Revisión
+'
+' Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión
+' utilizando el carácter "*", como se muestra a continuación:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
